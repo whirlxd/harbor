381dd4527bff2e89110200c5ac035842aa3dd252
Max Wofford <max@maxwofford.com>
2025-02-17 15:06:46 -0500

    Delete paper_trail.rb

--- config/initializers/paper_trail.rb
+++
@@ -1,9 +0,0 @@
-PaperTrail.config.track_associations = false
-PaperTrail.config.enabled = true
-
-Rails.application.config.to_prepare do
-  PaperTrail::Version.module_eval do
-    belongs_to :item, polymorphic: true
-    belongs_to :user, optional: true
-  end
-end
