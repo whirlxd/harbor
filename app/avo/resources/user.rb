9ab4ffba7301b5ea6fd12d84f330b3dcf40ad6e6
Max Wofford <max@maxwofford.com>
2025-02-21 17:40:08 -0500

    Update fields shown in avo

--- app/avo/resources/user.rb
+++ app/avo/resources/user.rb
@@ -7,10 +7,10 @@ class Avo::Resources::User < Avo::BaseResource
     field :email, as: :text
     field :username, as: :text
     field :slack_uid, as: :text
-    field :avatar_url, as: :text
     field :is_admin, as: :boolean
     field :created_at, as: :date_time, readonly: true
-    field :updated_at, as: :date_time, readonly: true
+    field :slack_scopes
+    field :uses_slack_status, as: :boolean
 
     # Show versions/history in the show page
     field :versions, as: :has_many
