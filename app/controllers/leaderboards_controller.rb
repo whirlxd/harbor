e6d165c67d4361650d1180b8788e6b2a2501ce8e
Max Wofford <max@maxwofford.com>
2025-02-21 19:16:46 -0500

    Handle if leaderboard is still generating

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,8 +2,13 @@ class LeaderboardsController < ApplicationController
   def index
     @leaderboard = Leaderboard.find_by(start_date: Date.current)
 
-    @entries = @leaderboard.entries
-      .includes(:user)
-      .order(total_seconds: :desc)
+    if @leaderboard.nil?
+      LeaderboardUpdateJob.perform_later
+      flash.now[:notice] = "Leaderboard is being updated..."
+    else
+      @entries = @leaderboard.entries
+        .includes(:user)
+        .order(total_seconds: :desc)
+    end
   end
 end
