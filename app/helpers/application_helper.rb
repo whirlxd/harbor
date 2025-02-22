51d649d95cd13d4ea5a222e74d5bb8ca5aa28d45
24c02 <163450896+24c02@users.noreply.github.com>
2025-02-22 15:30:41 -0500

    make admin tools admin toolier

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,6 @@ module ApplicationHelper
 
   def admin_tool(class_name = "", element = "div", **options, &block)
     return unless current_user&.is_admin?
-    concat content_tag(element, class: "#{class_name}", **options, &block)
+    concat content_tag(element, class: "admin-tool #{class_name}", **options, &block)
   end
 end
