81591cdf8b59be7dea60d15c45d5b7f34c374ef8
Max Wofford <max@maxwofford.com>
2025-02-23 20:52:06 -0500

    New flavortext

--- lib/flavor_text.rb
+++ lib/flavor_text.rb
@@ -39,18 +39,67 @@ class FlavorText
       "carrying the zero",
       "ganking the one before it gets carried",
       "spinning violently around the y-axis",
-      "serializing first-time coders experiences",
-      "tokenizing personal experiences",
+      "#{%w[tokenizing serializing stringifying].sample} #{[ "blood, sweat, & tears", "the human condition", "personal experiences", "elbow grease" ].sample}",
       "waking up the bits",
       "petting the bits",
       "testing patience",
-      "npm installing 3 of 27594 packages",
+      "[npm] now installing #{rand(3..7)} of #{rand(26_000..29_000)} packages",
       "spinning the rgbs",
       "Installing dependencies",
       "shoveling the overflowed pixels",
       ".split() === :large_blue_circle::large_green_circle::large_yellow_circle::large_orange_circle::red_circle::large_purple_circle:",
       "Are ya' winning, son?",
-      "Dropkicking the cache into the sun"
+      "Dropkicking the cache into the sun",
+      "[#{self.other_servers.sample}] starting on port #{self.common_ports.sample}",
+      "compressing the accountants"
+    ]
+  end
+
+  def self.common_ports
+    %w[
+      80
+      443
+      3000
+      3001
+      3002
+    ]
+  end
+
+  def self.other_servers
+    %w[
+      express
+      django
+      flask
+      ngrok
+      nextjs
+    ]
+  end
+
+  def self.other_languages
+    %w[
+      bash
+      bunjs
+      c
+      c#
+      c++
+      go
+      java
+      javascript
+      kotlin
+      perl
+      php
+      python
+      rust
+      swift
+    ]
+  end
+
+  def self.rare_loading_messages
+    [
+      "I would like to thank the academy...",
+      "If you really think about it, isn't coffee just refried bean water?",
+      "I'd be faster if I was written in #{self.other_languages.sample}",
+      "Loading better loading messages..."
     ]
   end
 end
