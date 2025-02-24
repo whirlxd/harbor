58b5fe3cc5573048e87b2c33e50b1f84adc0a0df
Max Wofford <max@maxwofford.com>
2025-02-23 23:36:18 -0500

    Dig incase slack username isn't valid

--- lib/slack_username.rb
+++ lib/slack_username.rb
@@ -3,7 +3,7 @@ class SlackUsername
     cached_name = Rails.cache.fetch("slack_username_#{uid}", expires_in: 1.day) do
       response = HTTP.headers(Authorization: "Bearer #{ENV.fetch("SAILORS_LOG_SLACK_BOT_OAUTH_TOKEN")}").get("https://slack.com/api/users.info?user=#{uid}")
       data = JSON.parse(response.body)
-      data["user"]["profile"]["display_name_normalized"]
+      data.dig("user", "profile", "display_name_normalized") || nil
     end
 
     cached_name
