1e9d0f22009b4cc1c3764e2bc0e4228edc7b0ba3
Max Wofford <max@maxwofford.com>
2025-02-24 21:21:40 -0500

    Add teletype printouts

---
+++ app/jobs/sailors_log_teletype_job.rb
@@ -0,0 +1,11 @@
+class SailorsLogTeletypeJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+    HTTP.auth("Bearer #{ENV['TELETYPE_API_KEY']}")
+      .post("https://printer.schmitworks.dev/api/raw",
+            body: {
+              text: message
+            })
+  end
+end
