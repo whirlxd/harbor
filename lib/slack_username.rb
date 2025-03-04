bfec8f5934e5e08e8541a78f220feff6b4d6b343
Max Wofford <max@maxwofford.com>
2025-03-04 17:20:07 -0500

    Default to username for leaderboard

--- lib/slack_username.rb
+++ lib/slack_username.rb
@@ -3,7 +3,11 @@ class SlackUsername
     cached_name = Rails.cache.fetch("slack_username_#{uid}", expires_in: 1.day) do
       response = HTTP.headers(Authorization: "Bearer #{ENV.fetch("SAILORS_LOG_SLACK_BOT_OAUTH_TOKEN")}").get("https://slack.com/api/users.info?user=#{uid}")
       data = JSON.parse(response.body)
-      data.dig("user", "profile", "display_name_normalized") || nil
+
+      name = data.dig("user", "profile", "display_name")
+      name ||= data.dig("user", "profile", "display_name_normalized")
+
+      name
     end
 
     cached_name
