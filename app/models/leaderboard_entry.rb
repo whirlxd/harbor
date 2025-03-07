08e944173c02ebf2af7232b1d9df41d2ac09eb67
Max Wofford <max@maxwofford.com>
2025-03-07 15:15:19 -0500

    Update validations on leaderboard entries

--- app/models/leaderboard_entry.rb
+++ app/models/leaderboard_entry.rb
@@ -2,7 +2,7 @@ class LeaderboardEntry < ApplicationRecord
   belongs_to :leaderboard
   belongs_to :user, primary_key: :slack_uid
 
-  validates :user_id, presence: true
+  validates :slack_uid, presence: true
   validates :total_seconds, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :user_id, uniqueness: { scope: :leaderboard_id }
+  validates :slack_uid, uniqueness: { scope: :leaderboard_id }
 end
