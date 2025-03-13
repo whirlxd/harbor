9b7f1d058bda2d6287a6f53880b0d4816cd25aea
Max Wofford <max@maxwofford.com>
2025-03-13 18:52:24 -0400

    Remove unused job concurrency key

--- app/jobs/wakatime_clear_test_heartbeats_job.rb
+++ app/jobs/wakatime_clear_test_heartbeats_job.rb
@@ -5,7 +5,6 @@ class WakatimeClearTestHeartbeatsJob < ApplicationJob
 
   # Limits concurrency to 1 job per date
   good_job_control_concurrency_with(
-    key: -> { arguments.first || Date.current.to_s },
     total: 1,
     drop: true
   )
