a06279d4c4bc37ba6248ffb3c9ada1f86f8576da
Max Wofford <max@maxwofford.com>
2025-03-15 01:05:02 -0400

    Remove 406 on old browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :initialize_cache_counters
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   helper_method :current_user, :user_signed_in?
 
