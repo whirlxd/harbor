f43f8a4a388cb1b2d69ce8553fb075dee361e5fd
Zach Latta <zach@zachlatta.com>
2025-03-15 17:48:41 -0400

    Add index on category and time for heartbeats

---
+++ db/migrate/20250315214819_add_index_category_time_heartbeats.rb
@@ -0,0 +1,5 @@
+class AddIndexCategoryTimeHeartbeats < ActiveRecord::Migration[8.0]
+  def change
+    add_index :heartbeats, [:category, :time], name: 'index_heartbeats_on_category_and_time'
+  end
+end
