dde1888860c60c84249c7efb68f7e46b5083972d
Zach Latta <zach@zachlatta.com>
2025-03-15 09:10:26 -0400

    Don't take down the site

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -45,21 +45,28 @@ class StaticPagesController < ApplicationController
       @todays_editors = editor_counts.map(&:first)
       @show_logged_time_sentence = @todays_languages.any? || @todays_editors.any?
     else
-      in_past_week = Heartbeat.where("time > ?", 1.week.ago.to_f).distinct.count(:user_id)
-      in_past_day = Heartbeat.where("time > ?", 1.day.ago.to_f).distinct.count(:user_id)
-      in_past_hour = Heartbeat.where("time > ?", 1.hour.ago.to_f).distinct.count(:user_id)
-      @social_proof ||= begin
-        if in_past_hour > 5
-          "In the past hour, #{in_past_hour} Hack Clubbers have coded with Hackatime."
-        elsif in_past_day > 5
-          "In the past day, #{in_past_day} Hack Clubbers have coded with Hackatime."
-        elsif in_past_week > 5
-          "In the past week, #{in_past_week} Hack Clubbers have coded with Hackatime."
+      begin
+        in_past_week = Heartbeat.where("time > ?", 1.week.ago.to_f).distinct.count(:user_id)
+        in_past_day = Heartbeat.where("time > ?", 1.day.ago.to_f).distinct.count(:user_id)
+        in_past_hour = Heartbeat.where("time > ?", 1.hour.ago.to_f).distinct.count(:user_id)
+        @social_proof ||= begin
+          if in_past_hour > 5
+            "In the past hour, #{in_past_hour} Hack Clubbers have coded with Hackatime."
+          elsif in_past_day > 5
+            "In the past day, #{in_past_day} Hack Clubbers have coded with Hackatime."
+          elsif in_past_week > 5
+            "In the past week, #{in_past_week} Hack Clubbers have coded with Hackatime."
+          end
         end
-      end
 
-      @users_tracked = Heartbeat.distinct.count(:user_id)
-      @hours_tracked = Heartbeat.duration_seconds / 3600
+        @users_tracked = Heartbeat.distinct.count(:user_id)
+        @hours_tracked = Heartbeat.duration_seconds / 3600
+      rescue ActiveRecord::StatementInvalid => e
+        # Handle timestamp overflow errors
+        Rails.logger.error "Error calculating statistics: #{e.message}"
+        @users_tracked = Heartbeat.distinct.count(:user_id) rescue 0
+        @hours_tracked = 0
+      end
     end
   end
 
@@ -103,33 +110,39 @@ class StaticPagesController < ApplicationController
   private
 
   def get_setup_social_proof
-    # Count users who set up in different time periods
-    in_past_5_min = Heartbeat.where("time > ? AND source_type = ?", 5.minutes.ago.to_f, Heartbeat.source_types[:test_entry])
+    begin
+      # Count users who set up in different time periods
+      in_past_5_min = Heartbeat.where("time > ? AND source_type = ?", 5.minutes.ago.to_f, Heartbeat.source_types[:test_entry])
+                              .distinct.count(:user_id)
+      in_past_hour = Heartbeat.where("time > ? AND source_type = ?", 1.hour.ago.to_f, Heartbeat.source_types[:test_entry])
+                             .distinct.count(:user_id)
+      in_past_day = Heartbeat.where("time > ? AND source_type = ?", 1.day.ago.to_f, Heartbeat.source_types[:test_entry])
                             .distinct.count(:user_id)
-    in_past_hour = Heartbeat.where("time > ? AND source_type = ?", 1.hour.ago.to_f, Heartbeat.source_types[:test_entry])
-                           .distinct.count(:user_id)
-    in_past_day = Heartbeat.where("time > ? AND source_type = ?", 1.day.ago.to_f, Heartbeat.source_types[:test_entry])
+      in_past_week = Heartbeat.where("time > ? AND source_type = ?", 1.week.ago.to_f, Heartbeat.source_types[:test_entry])
+                             .distinct.count(:user_id)
+      in_past_month = Heartbeat.where("time > ? AND source_type = ?", 1.month.ago.to_f, Heartbeat.source_types[:test_entry])
+                              .distinct.count(:user_id)
+      this_year = Heartbeat.where("time > ? AND source_type = ?", Time.current.beginning_of_year.to_f, Heartbeat.source_types[:test_entry])
                           .distinct.count(:user_id)
-    in_past_week = Heartbeat.where("time > ? AND source_type = ?", 1.week.ago.to_f, Heartbeat.source_types[:test_entry])
-                           .distinct.count(:user_id)
-    in_past_month = Heartbeat.where("time > ? AND source_type = ?", 1.month.ago.to_f, Heartbeat.source_types[:test_entry])
-                            .distinct.count(:user_id)
-    this_year = Heartbeat.where("time > ? AND source_type = ?", Time.current.beginning_of_year.to_f, Heartbeat.source_types[:test_entry])
-                        .distinct.count(:user_id)
 
-    # Choose the most appropriate time period based on user count
-    if in_past_5_min >= 1
-      "#{in_past_5_min} Hack Clubber#{in_past_5_min > 1 ? 's' : ''} set up Hackatime in the past 5 minutes"
-    elsif in_past_hour >= 3
-      "#{in_past_hour} Hack Clubbers set up Hackatime in the past hour"
-    elsif in_past_day >= 5
-      "#{in_past_day} Hack Clubbers set up Hackatime in the past day"
-    elsif in_past_week >= 5
-      "#{in_past_week} Hack Clubbers set up Hackatime in the past week"
-    elsif in_past_month >= 5
-      "#{in_past_month} Hack Clubbers set up Hackatime in the past month"
-    else
-      "#{this_year} Hack Clubbers set up Hackatime this year"
+      # Choose the most appropriate time period based on user count
+      if in_past_5_min >= 1
+        "#{in_past_5_min} Hack Clubber#{in_past_5_min > 1 ? 's' : ''} set up Hackatime in the past 5 minutes"
+      elsif in_past_hour >= 3
+        "#{in_past_hour} Hack Clubbers set up Hackatime in the past hour"
+      elsif in_past_day >= 5
+        "#{in_past_day} Hack Clubbers set up Hackatime in the past day"
+      elsif in_past_week >= 5
+        "#{in_past_week} Hack Clubbers set up Hackatime in the past week"
+      elsif in_past_month >= 5
+        "#{in_past_month} Hack Clubbers set up Hackatime in the past month"
+      else
+        "#{this_year} Hack Clubbers set up Hackatime this year"
+      end
+    rescue ActiveRecord::StatementInvalid => e
+      # Handle timestamp overflow errors
+      Rails.logger.error "Error calculating setup social proof: #{e.message}"
+      "Join the community of Hack Clubbers using Hackatime"
     end
   end
 end
