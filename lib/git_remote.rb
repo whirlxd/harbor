c7f2d687b2f494aa248b9b25f7bf04c96e59a2d3
Max Wofford <max@maxwofford.com>
2025-04-30 18:33:28 -0400

    Skip repo check if git isn't in path

--- lib/git_remote.rb
+++ lib/git_remote.rb
@@ -2,6 +2,9 @@ require "open3"
 
 class GitRemote
   def self.check_remote_exists(repo_url)
+    # only run check if git is installed and in path
+    return true unless system("git --version")
+
     safe_repo_url = URI.parse(repo_url).to_s.gsub(" ", "").gsub("'", "")
     Open3.capture2e("git", "ls-remote", safe_repo_url).last.success?
   end
