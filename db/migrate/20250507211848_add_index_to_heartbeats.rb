62a7a8a7a3b8ca4d0bf835aed396ca5a80d4b2a0
Max Wofford <max@maxwofford.com>
2025-05-07 17:20:45 -0400

    Add user+time index to heartbeats

---
+++ db/migrate/20250507211848_add_index_to_heartbeats.rb
@@ -0,0 +1,7 @@
+class AddIndexToHeartbeats < ActiveRecord::Migration[8.0]
+  def change
+    add_index :heartbeats, [ :user_id, :time ],
+              name: "idx_heartbeats_user_time_active",
+              where: "deleted_at IS NULL"
+  end
+end
