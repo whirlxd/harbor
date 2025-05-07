eccd1acc9ba9a16589fcd951ce98903171e2de26
Max Wofford <max@maxwofford.com>
2025-05-07 17:21:34 -0400

    Only run if not exists

--- db/migrate/20250507211848_add_index_to_heartbeats.rb
+++ db/migrate/20250507211848_add_index_to_heartbeats.rb
@@ -2,6 +2,7 @@ class AddIndexToHeartbeats < ActiveRecord::Migration[8.0]
   def change
     add_index :heartbeats, [ :user_id, :time ],
               name: "idx_heartbeats_user_time_active",
-              where: "deleted_at IS NULL"
+              where: "deleted_at IS NULL",
+              if_not_exists: true
   end
 end
