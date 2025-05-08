c52c24591dbb95e807d8836ddb21fc130b11a19b
Max Wofford <max@maxwofford.com>
2025-05-08 18:22:26 -0400

    Turn geocoding back on for ahoy

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -7,6 +7,6 @@ Ahoy.api = false
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
-Ahoy.geocode = false
+Ahoy.geocode = true
 
 Ahoy.job_queue = :latency_1s
