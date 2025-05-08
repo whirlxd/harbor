165c19cf9aa4ce019422557eadff9a06ce809332
Max Wofford <max@maxwofford.com>
2025-05-08 15:40:18 -0400

    Make cleanup email verification request a single queue

--- app/jobs/cleanup_expired_email_verification_requests_job.rb
+++ app/jobs/cleanup_expired_email_verification_requests_job.rb
@@ -1,6 +1,13 @@
 class CleanupExpiredEmailVerificationRequestsJob < ApplicationJob
   queue_as :interval_10s
 
+  include GoodJob::ActiveJobExtensions::Concurrency
+
+  good_job_control_concurrency_with(
+    total_limit: 1,
+    drop: true
+  )
+
   def perform
     # Soft delete all expired and non-deleted verification requests in a single query
     EmailVerificationRequest.expired.where(deleted_at: nil)
