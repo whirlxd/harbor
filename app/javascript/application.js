436602f49ecd1d9343faa7505db75a1356755c1e
Max Wofford <max@maxwofford.com>
2025-05-08 03:36:55 -0400

    Attempt to speed up currently hacking by loading in count upfront (#212)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+  const container = document.querySelector('.currently-hacking-container');
+  const header = container?.querySelector('.currently-hacking-header');
+  if (container && header) {
+    header.addEventListener('click', function() {
+      container.classList.toggle('visible');
+    });
+  }
+});
