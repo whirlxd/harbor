3ccc1c43b669581b208e3f01d893131d6cfd3057
Max Wofford <max@maxwofford.com>
2025-05-08 12:37:10 -0400

    Fix misuse of heartbeat counts cache job

--- app/models/heartbeat.rb
+++ app/models/heartbeat.rb
@@ -90,11 +90,11 @@ class Heartbeat < ApplicationRecord
   validates :time, presence: true
 
   def self.recent_count
-    Cache::HeartbeatCountsJob.new.calculate[:recent_count]
+    Cache::HeartbeatCountsJob.calculate[:recent_count]
   end
 
   def self.recent_imported_count
-    Cache::HeartbeatCountsJob.new.calculate[:recent_imported_count]
+    Cache::HeartbeatCountsJob.calculate[:recent_imported_count]
   end
 
   def self.generate_fields_hash(attributes)
