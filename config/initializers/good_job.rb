2aac353255f432816ee88768989c2fac6086f0f7
Max Wofford <max@maxwofford.com>
2025-05-08 19:31:29 -0400

    Update good_job.rb

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
   config.good_job.preserve_job_records = true
   config.good_job.cleanup_preserved_jobs_before_seconds_ago = 60 * 60 * 24 * 7
   config.good_job.cleanup_interval_jobs = 1000
+  config.good_job.cleanup_interval_seconds = 3600
 
   config.good_job.enable_cron = true
   config.good_job.execution_mode = :async
