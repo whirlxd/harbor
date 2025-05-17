cbaece8b655c25184362a46e63d6f82500c40a2a
Max Wofford <max@maxwofford.com>
2025-05-16 23:35:15 -0400

    Require authentication for mailroom controller

--- app/controllers/my/mailroom_controller.rb
+++ app/controllers/my/mailroom_controller.rb
@@ -1,9 +1,17 @@
 module My
   class MailroomController < ApplicationController
+    before_action :ensure_current_user
+
     def index
       @user = current_user
       @physical_mails = @user.physical_mails.order(created_at: :desc)
       @has_mailing_address = @user.mailing_address.present?
+    end
+
+    private
+
+    def ensure_current_user
+      redirect_to root_path, alert: "You must be logged in to view this page" unless current_user
     end
   end
 end
