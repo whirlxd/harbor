5378a46863bdc6436ddd05822e015654e3ae6dba
Max Wofford <max@maxwofford.com>
2025-05-22 17:20:55 -0400

    Add timeouts in neighborhood airtable sync

--- app/jobs/neighborhood/sync_from_airtable_job.rb
+++ app/jobs/neighborhood/sync_from_airtable_job.rb
@@ -3,8 +3,14 @@ class Neighborhood::SyncFromAirtableJob < ApplicationJob
 
   def perform
     Neighborhood::App.pull_all_from_airtable!
+    sleep 10
+
     Neighborhood::Project.pull_all_from_airtable!
+    sleep 10
+
     Neighborhood::Post.pull_all_from_airtable!
+    sleep 10
+
     Neighborhood::YswsSubmission.pull_all_from_airtable!
   end
 end
