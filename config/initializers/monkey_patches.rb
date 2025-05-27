813d2ac9ae714a2e73896d91e2948ba0e113deb4
Max Wofford <max@maxwofford.com>
2025-05-27 15:39:27 -0400

    Monkeypatch lower airtable ratelimit

--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -10,4 +10,8 @@ Rails.configuration.to_prepare do
       end
     end
   end
+
+  # Monkeypatch Airtable rate limit to be more conservative
+  Norairrecord::Client.send(:remove_const, :AIRTABLE_RPS_LIMIT) if Norairrecord::Client.const_defined?(:AIRTABLE_RPS_LIMIT)
+  Norairrecord::Client.const_set(:AIRTABLE_RPS_LIMIT, 2) # Set to 2 requests per second
 end
