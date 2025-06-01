aa339608204b1d73c647060cd8f9b4948ad9da83
Max Wofford <max@maxwofford.com>
2025-06-01 12:44:22 -0400

    Remove project posts from timeline

--- app/controllers/admin/timeline_controller.rb
+++ app/controllers/admin/timeline_controller.rb
@@ -5,37 +5,6 @@ class Admin::TimelineController < Admin::BaseController
     # for span calculations (visual blocks in the timeline)
     timeout_duration = 10.minutes.to_i # This remains for span display
 
-    @review_item = Neighborhood::Post.find_by(airtable_id: params["post_id"]) if params["post_id"].present?
-    # all posts from user
-    @posts_from_user = Neighborhood::Post.where(airtable_fields: { slackId: @review_item.airtable_fields["slackId"] }) if @review_item.present?
-
-    # Define primary_user_tz early for use in timezone conversions
-    primary_user_tz = current_user&.timezone || "UTC"
-
-    if @review_item.present?
-      puts "review item found"
-      puts @review_item.airtable_fields
-      created_at = Time.parse(@review_item.airtable_fields["createdAt"])
-      user_tz = @review_item_user&.timezone || primary_user_tz
-      created_at_in_user_tz = created_at.in_time_zone(user_tz)
-      @date = created_at_in_user_tz.to_date
-
-      @slack_uid = @review_item.airtable_fields["slackId"]&.first
-      @review_item_user = User.find_by(slack_uid: @slack_uid) if @slack_uid.present?
-
-      if @review_item.present? && @review_item_user.present?
-        @review_item_marker = {
-          user_id: @review_item_user.id,
-          timestamp: Time.parse(@review_item.airtable_fields["createdAt"]).to_f,
-          description: @review_item.airtable_fields["description"],
-          demo_video: @review_item.airtable_fields["demoVideo"],
-          photobooth_video: @review_item.airtable_fields["photoboothVideo"],
-          hackatime_time: @review_item.airtable_fields["hackatimeTime"],
-          hackatime_time_hours: @review_item.airtable_fields["hackatimeTimeHours"]
-        }
-      end
-    end
-
     # Determine the date to display (default to today)
     @date ||= params[:date] ? Date.parse(params[:date]) : Time.current.to_date
 
@@ -55,7 +24,6 @@ class Admin::TimelineController < Admin::BaseController
 
     # Always include current_user (admin)
     @selected_user_ids = [ current_user.id ] + raw_user_ids
-    @selected_user_ids << @review_item_user.id if @review_item_user.present?
     @selected_user_ids.uniq!
 
     user_ids_to_fetch = @selected_user_ids
@@ -161,45 +129,7 @@ class Admin::TimelineController < Admin::BaseController
 
     @primary_user = @users_with_timeline_data.first&.[](:user) || current_user
 
-    # Get slack_uids for all selected users
-    slack_uids = User.where(id: @selected_user_ids).pluck(:slack_uid).compact
-    date_str = @date.to_s
 
-    # Get posts from selected users that intersect with the current date
-    # A post is active on a date if it was created after that date AND its last post was before that date
-    posts_for_timeline = Neighborhood::Post.where(
-      "airtable_fields -> 'slackId' ?| array[:slack_uids]",
-      slack_uids: slack_uids
-    ).where(
-      "DATE(airtable_fields ->> 'createdAt') >= ? AND " \
-      "DATE(airtable_fields ->> 'lastPost') <= ?",
-      date_str, date_str
-    )
-
-    @timeline_post_markers = posts_for_timeline.map do |post|
-      user = User.find_by(slack_uid: post.airtable_fields["slackId"]&.first)
-      next unless user
-
-      # Parse the createdAt time and convert to user's timezone
-      created_at = Time.parse(post.airtable_fields["createdAt"])
-      user_tz = user.timezone || primary_user_tz
-      created_at_in_user_tz = created_at.in_time_zone(user_tz)
-
-      # Cap the timestamp at the end of the current day in user's timezone
-      timestamp = [ created_at_in_user_tz.to_f, @date.end_of_day.in_time_zone(user_tz).to_f ].min
-
-      {
-        user_id: user.id,
-        timestamp: timestamp,
-        description: post.airtable_fields["description"],
-        demo_video: post.airtable_fields["demoVideo"],
-        photobooth_video: post.airtable_fields["photoboothVideo"],
-        hackatime_time: post.airtable_fields["hackatimeTime"],
-        hackatime_time_hours: post.airtable_fields["hackatimeTimeHours"],
-        highlighted: (@review_item.present? && post.id == @review_item.id),
-        airtable_url: post.airtable_url
-      }
-    end.compact
 
     # Get commit markers for timeline
     commits_for_timeline = Commit.where(
