3204e41be8c68dc031be4ebd0d8713827059b447
Max Wofford <max@maxwofford.com>
2025-06-09 10:52:30 -0400

    Rubocop format

--- db/migrate/20250609144356_add_index_to_commits_user_id_created_at.rb
+++ db/migrate/20250609144356_add_index_to_commits_user_id_created_at.rb
@@ -2,6 +2,6 @@ class AddIndexToCommitsUserIdCreatedAt < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    add_index :commits, [:user_id, :created_at], algorithm: :concurrently
+    add_index :commits, [ :user_id, :created_at ], algorithm: :concurrently
   end
 end
