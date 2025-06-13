b9a3fbae8fdaa3a003ab647faf4b1d3f0095b93e
Max Wofford <max@maxwofford.com>
2025-06-13 16:06:45 -0400

    Remove non-working monkeypatch for throttling

--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -11,9 +11,5 @@ Rails.configuration.to_prepare do
     end
   end
 
-  # Monkeypatch Airtable rate limit to be more conservative
-  Norairrecord::Client.send(:remove_const, :AIRTABLE_RPS_LIMIT) if Norairrecord::Client.const_defined?(:AIRTABLE_RPS_LIMIT)
-  Norairrecord::Client.const_set(:AIRTABLE_RPS_LIMIT, 1) # Set to 1 requests per second
-
   Doorkeeper::ApplicationsController.layout "application" # show oauth2 admin in normal hackatime ui
 end
