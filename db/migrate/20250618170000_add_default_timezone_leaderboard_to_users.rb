5a3bcf2090f274247be5e6a2bc6a1bfb5aa863d5
Echo <github@3kh0.net>
2025-06-18 16:14:14 -0400

    if not exists

--- db/migrate/20250618170000_add_default_timezone_leaderboard_to_users.rb
+++ db/migrate/20250618170000_add_default_timezone_leaderboard_to_users.rb
@@ -1,5 +1,5 @@
 class AddDefaultTimezoneLeaderboardToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :default_timezone_leaderboard, :boolean, default: true, null: false
+    add_column :users, :default_timezone_leaderboard, :boolean, default: true, null: false, if_not_exists: true
   end
 end
