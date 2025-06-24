ff863b328b3cd89896024ca743356ef9fe44389f
Echo <github@3kh0.net>
2025-06-24 17:29:13 -0400

    fix borders

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/**/*',
   ],
   theme: {
     extend: {
