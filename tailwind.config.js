da41bf3aa68f230e8fc29ca3c2c795cf5f441f8b
Echo <github@3kh0.net>
2025-06-24 17:12:13 -0400

    rework color displays

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'darker': '#121217',
+        'dark': '#17171d',
+        'darkless': '#252429',
+        'black': '#1f2d3d',
+        'steel': '#273444',
+        'slate': '#3c4858',
+        'muted': '#8492a6',
+        'smoke': '#e0e6ed',
+        'snow': '#f9fafc',
+        'white': '#ffffff',
+        'red': '#ec3750',
+        'orange': '#ff8c37',
+        'yellow': '#f1c40f',
+        'green': '#33d6a6',
+        'cyan': '#5bc0de',
+        'blue': '#338eda',
+        'purple': '#a633d6',
+        'primary': '#ec3750',
+        'secondary': '#8492a6',
+        'accent': '#5bc0de',
+        'text': '#ffffff',
+        'background': '#121217',
+        'elevated': '#17171d',
+        'sheet': '#252429',
+        'sunken': '#252429',
+        'border': '#252429'
+      }
+    },
+  },
+  plugins: [],
+}
