8eb607261278eae52c4008067387ac8ad09b9224
Max Wofford <max@maxwofford.com>
2025-06-27 21:26:29 -0400

    Take 2 of switching to psql cache!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_001500) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_28_011017) do
   create_schema "pganalyze"
 
   # These are extensions that must be enabled in order to support this database
@@ -470,6 +470,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_26_001500) do
     t.jsonb "return_data"
     t.index ["token"], name: "index_sign_in_tokens_on_token"
     t.index ["user_id"], name: "index_sign_in_tokens_on_user_id"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "users", force: :cascade do |t|
